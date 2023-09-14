9aad5f6cca90b66f6f812611e7a5c60650d675d9
smmrue <psjung0826@naver.com>
2023-09-15 00:06:13 +0900

    week2

---
+++ Week2/src/Pizza.java
@@ -0,0 +1,26 @@
+class Circle {
+	protected int radius;
+	
+	protected Circle(int r) {
+		radius = r;
+	}
+}
+
+public class Pizza extends Circle {
+	String menu;
+	
+	public Pizza(String p, int r) {
+		super(r);
+		this.menu = p;
+	}
+	
+	public void print() {
+		System.out.println("피자의 종류 : " + this.menu + "," + "피자의 크기 : " + super.radius);
+		
+	}
+	
+	public static void main(String[] args) {
+		Pizza obj = new Pizza("Pepperoni", 20);
+		obj.print();
+	}
+}
