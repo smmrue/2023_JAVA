6475837cf4cb92af2cb14a22dcdd7272c5730d68
smmrue <psjung0826@naver.com>
2023-10-29 23:49:23 +0900

    7주차 과제

--- Week6_smmrue/src/MethodReferenceLowerCase.java
+++ Week6_smmrue/src/MethodReferenceLowerCase.java
@@ -1,19 +1,19 @@
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class MethodReferenceLowerCase {
 
 	public static void main(String[] args) {
-		List<String> listOfNames = Arrays.asList(new String[]
-				{"Apple","Banana","Cherry"});
-		
-		 List<String> lowercaseList = listOfNames.stream()
-	                .map(String::toLowerCase)
-	                .collect(Collectors.toList());
+		List<String> listOfNames = Arrays.asList(new String[] 
+                {"Apple","Banana","Cherry"});
 
-	        System.out.println(lowercaseList);
-		
+		Function<String,String> function1= (name) -> name.toLowerCase();
+		ArrayList<String> collect1 = listOfNames.stream().map(function1)
+	        .collect(Collectors.toCollection(ArrayList::new));
+		System.out.println(collect1);
 	}
-
 }
